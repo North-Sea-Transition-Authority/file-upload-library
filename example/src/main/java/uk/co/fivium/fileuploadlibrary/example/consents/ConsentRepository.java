9f15f231464e2b3ac592b79dfdcafb9c7326dae3
Dainis <44690511+dgrinbergs@users.noreply.github.com>
2023-06-22 15:19:53 +0100

    removed example project (#40)

--- example/src/main/java/uk/co/fivium/fileuploadlibrary/example/consents/ConsentRepository.java
+++
@@ -1,7 +0,0 @@
-package uk.co.fivium.fileuploadlibrary.example.consents;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-interface ConsentRepository extends JpaRepository<ConsentDetail, Long> {
-
-}
--- example/src/main/resources/db/migration/V1__create_consent_details_table.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE consent_details (
-    id BIGSERIAL PRIMARY KEY
-);
